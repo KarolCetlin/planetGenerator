6c95e37013a69b7cc8e4450d54aed94ef6c83656
Karol Cetlin <cetlin.karol@gmail.com>
2019-11-15 12:53:47 +0100

    NewStar class to create stars

---
+++ JS/NewStar.js
@@ -0,0 +1,37 @@
+'use strict';
+
+class Star {
+    constructor(){
+        this.starNameHTML = document.getElementById('starName');
+        this.starSpectralTypeHTML = document.getElementById('starSpectreType');
+
+
+    }
+
+
+    initStar(){
+
+        let drawNameForStar = this.calc.drawPropertyFromArray(this.data.starName);
+        this.starNameHTML.textContent = this.data.starName[drawNameForStar];
+
+        let drawSizeForStar = this.calc.drawPropertyFromArray(this.data.starType);
+        this.starType.innerHTML = `<span>Wielkość gwiazdy:</span> ${this.data.starType[drawSizeForStar].name}`;
+
+        let drawNumberFromThisRange = this.drawStarTemeperature(drawSizeForStar);
+
+        this.starSpectralTypeHTML.innerHTML = `<span>Temperatura barmowa:</span> ${this.data.starSpectralType[drawNumberFromThisRange].name}`;
+
+
+
+    }
+
+    drawStarTemeperature(starNumber){
+        let minSpectral = this.data.starType[starNumber].minSpectralType;
+        let maxSpectral = this.data.starType[starNumber].maxSpectralType;
+
+        return Math.floor(Math.random() * (maxSpectral - minSpectral)) + minSpectral;
+
+    }
+
+
+}
