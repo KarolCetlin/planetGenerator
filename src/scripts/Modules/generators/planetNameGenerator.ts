fbe7058d4854e32b1dacec61964216020b6231fc
Kamil Pietrzak <kpietrzak.web@gmail.com>
2020-02-12 21:30:36 +0100

    added planet name generator

---
+++ src/scripts/Modules/generators/planetNameGenerator.ts
@@ -0,0 +1,17 @@
+const availablePlanetNames = ['Helara',
+  'Kitrarth',
+  'Zeceitera',
+  'Chemion',
+  '51 Pegasi b',
+  'PSR 1257+12 C',
+  'OGLE-TR-56b',
+  'Fomalhaut',
+  '2M1207b',
+];
+
+export const generatePlanetName = (): string => {
+  const availablePlanetsCount = availablePlanetNames.length;
+  const randomIndex = Math.floor(Math.random() * availablePlanetsCount - 1);
+
+  return availablePlanetNames[ randomIndex ];
+};
